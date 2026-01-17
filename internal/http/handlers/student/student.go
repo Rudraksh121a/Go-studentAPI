47d7abfbedb4b644caea056504942854cf0524a8
rudraksh121a <rudrakshkanungo2022@gmail.com>
2026-01-18 00:36:15 +0530

    GetStudentList endpoint added

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -74,4 +74,19 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 		response.WriteJson(w, http.StatusOK, student)
 
 	}
+
+}
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Getting the list of students")
+
+		students, err := storage.GetStudentList()
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+		response.WriteJson(w, http.StatusOK, students)
+	}
 }
